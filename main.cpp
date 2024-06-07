c9eeb6e481ee53585b7a9f1f014e6279c87cd7f6
joon <joon@snu.ac.kr>
2024-06-07 07:49:11 +0000

    modify func

--- main.cpp
+++ main.cpp
@@ -7,16 +7,25 @@ int N,M;
 vector<int> vec;
 
 void func(int cnt) {
+	if (cnt == M){
+		for (int number : vec){
+			cout << number << " ";
+		}
+		cout << "\n";
+		return;
+	}
 
+	int strt = vec.empty() ? 1: vec.back()+1;
 
-
-
-
-
-
+	for (int i=strt; i<=N; i++){
+	vec.push_back(i);
+	func(cnt+1);
+	vec.pop_back();
+	}
 }
 
 int main() {
    cin >> N >> M;
    func(0);
+   return 0;
 }
